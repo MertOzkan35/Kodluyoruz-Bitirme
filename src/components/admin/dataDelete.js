fac8d7fdd392b02ff63d9baed1f226a98e0e94b3
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-09-12 12:58:52 +0300

    admin panel processes

--- src/components/admin/dataDelete.js
+++ src/components/admin/dataDelete.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { deleteData } from "../../store/slices/dataSlices";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export class DataDelete extends Component {
@@ -32,14 +33,61 @@ export class DataDelete extends Component {
             SALES INFO
           </Link>
         </div>
-        <div className="w-3/4 h-72 bg-slate-500"></div>
+        <div className="w-3/4 h-full ">
+          <div className=" grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-12 m-16 ">
+            {this.props.allData &&
+              this.props.allData.map((element, key) => {
+                return (
+                  <div
+                    key={key}
+                    className="w-full h-full border  p-4  hover:border-[#fa9d28] rounded-xl flex flex-col hover:shadow-2xl"
+                  >
+                    <Link key={key} to={`/prod/${element.Id}`}>
+                      <p className="w-full text-[#4c4c4c] text-end font-bold">
+                        {" "}
+                        Kullanıcı Puanı : {element.Point}
+                      </p>
+                      <img
+                        className=" rounded-xl object-cover"
+                        src={element.img}
+                      />
+
+                      <p className="font-bold text-[#207609] text-center">
+                        {element.Stok}
+                      </p>
+                      <p className="text-black hover:text-[#fa9d28] text-center text-xl font-bold mt-8">
+                        {element.Name}
+                      </p>
+                      <p className="text-[#001d48] text-center text-xl font-bold mt-12">
+                        {element.Price + ",00 TL"}
+                      </p>
+                    </Link>
+                    <div className="w-full flex justify-center items-center pt-8">
+                      <button
+                        // onClick={(e) => shoppingCart(`${element.Id}`)}
+                        onClick={() => this.props.deleteData(`${element.Id}`)}
+                        className="w-1/2  h-12 bg-[#fa9d28]  hover:border-2 rounded-2xl border font-bold"
+                      >
+                        Ürünü Sil
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({});
+function mapStateToProps(data) {
+  const allData = data.data.data;
+  // const ProdId = allData.data.length.toString();
+  console.log(allData);
+  return { allData };
+}
 
 const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(DataDelete);
+export default connect(mapStateToProps, { deleteData })(DataDelete);
--- src/components/admin/dataUpdate.js
+++ src/components/admin/dataUpdate.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { updateData } from "../../store/slices/dataSlices";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export class DataUpdate extends Component {
+  state = {
+    Point: "",
+    Name: "",
+    Stok: "",
+    Id: "",
+    img: "",
+    Category: "",
+    Price: 0,
+    Features: "",
+  };
+  componentDidUpdate(pP, state) {
+    state = state;
+    console.log(this.state);
+  }
+  createProdName(event) {
+    this.setState({ Name: event.target.value });
+  }
+  createProdId(event) {
+    this.setState({ Id: event.target.value });
+  }
+
+  createProdStok(event) {
+    this.setState({ Stok: event.target.value });
+  }
+  createProdFeatures(event) {
+    this.setState({ Features: event.target.value });
+  }
+  createProdPrice(event) {
+    const toNumber = event.target.value;
+    this.setState({ Price: Number(toNumber) });
+  }
+  createProdCategory(event) {
+    this.setState({ Category: event.target.value });
+  }
+  createProdImage(event) {
+    this.setState({ img: event.target.value });
+  }
+  createProdPoint(event) {
+    this.setState({ Point: event.target.value });
+  }
   render() {
     return (
       <div className="w-full flex flex-cols-2">
@@ -32,14 +73,118 @@ export class DataUpdate extends Component {
             SALES INFO
           </Link>
         </div>
-        <div className="w-3/4 h-72 bg-slate-500"></div>
+        <div className="w-3/4 h-full flex flex-col justify-center items-center">
+          {this.props.allData &&
+            this.props.allData.map((element, key) => {
+              return (
+                <div key={key} className="flex flex-col">
+                  <div className="w-3/4 h-full border-x border-t flex flex-cols-2  justify-between ">
+                    <div className=" w-full h-full border-r flex flex-col justify-between items-center gap-8  p-4">
+                      <div className="flex w-3/4 flex-col border-2 rounded-2xl mt-6   justify-center place-items-center ">
+                        <p className="font-bold pt-2 items-center ">Name</p>
+                        <textarea
+                          onChange={this.createProdName.bind(this)}
+                          className="w-3/4  font-semibold h-16 pt-2 border-2 rounded-2xl m-4 pl-2"
+                          placeholder={element.Name}
+                        ></textarea>
+                      </div>
+                      <div className="flex w-3/4 flex-col border-2 rounded-2xl mt   justify-center place-items-center ">
+                        <p className="font-bold pt-2 items-center ">Id</p>
+                        <p className=" pt-2  text-xs items-center ">
+                          'placeholder ile aynı olmalı'
+                        </p>
+                        <textarea
+                          onChange={this.createProdId.bind(this)}
+                          className="w-3/4  font-semibold h-16 pt-2 border-2 rounded-2xl m-4 pl-2"
+                          placeholder={element.Id}
+                        ></textarea>
+                      </div>
+
+                      <div className="flex w-3/4 flex-col border-2 rounded-2xl  justify-center place-items-center ">
+                        <p className="font-bold pt-2 items-center ">Category</p>
+                        <textarea
+                          onChange={this.createProdCategory.bind(this)}
+                          className="w-3/4 font-semibold h-16 pt-2 border-2 rounded-2xl m-4 pl-2"
+                          placeholder={element.Category}
+                        ></textarea>
+                      </div>
+                      <div className="flex w-3/4 flex-col border-2 rounded-2xl  justify-center place-items-center ">
+                        <p className="font-bold pt-2 items-center ">Price</p>
+                        <textarea
+                          type="number"
+                          onChange={this.createProdPrice.bind(this)}
+                          className="w-3/4 font-semibold h-16 pt-2 border-2 rounded-2xl m-4 pl-2"
+                          placeholder={element.Price}
+                        ></textarea>
+                      </div>
+                    </div>
+                    <div className=" w-full h-full flex flex-col  items-center gap-4 p-4 ">
+                      <div className="flex w-3/4 flex-col border-2 rounded-2xl mt-6  justify-start place-items-center ">
+                        <p className="font-bold pt-2 items-center ">Stok</p>
+                        <textarea
+                          onChange={this.createProdStok.bind(this)}
+                          className="w-3/4 font-semibold h-16 pt-2 border-2 rounded-2xl m-4 pl-2"
+                          placeholder={element.Stok}
+                        ></textarea>
+                      </div>
+                      <div className="flex w-3/4 flex-col border-2 rounded-2xl  justify-center place-items-center ">
+                        <p className="font-bold pt-2 items-center ">Point</p>
+                        <textarea
+                          onChange={this.createProdPoint.bind(this)}
+                          className="w-3/4 font-semibold h-16 pt-2 border-2 rounded-2xl m-4 pl-2"
+                          placeholder={element.Point}
+                        ></textarea>
+                      </div>
+                      <div className="flex w-3/4 flex-col border-2 rounded-2xl mt-4  justify-center place-items-center ">
+                        <p className="font-bold pt-2 items-center ">Image</p>
+                        <textarea
+                          onChange={this.createProdImage.bind(this)}
+                          className="w-3/4 font-semibold h-16 pt-2 border-2 rounded-2xl m-4 pl-2"
+                          placeholder={element.img}
+                        ></textarea>
+                      </div>
+                      <div className="flex w-3/4 flex-col border-2 rounded-2xl mt-6  justify-center place-items-center ">
+                        <p className="font-bold pt-2 items-center ">Features</p>
+                        <textarea
+                          onChange={this.createProdFeatures.bind(this)}
+                          rows="4"
+                          cols="50"
+                          className="w-3/4 font-semibold h-[180px] border-2 rounded-2xl pt-2 m-4 pl-2"
+                          placeholder={element.Features}
+                        ></textarea>
+                      </div>
+                      <div className="flex w-3/4 flex-col mt-8 mb-6    justify-center place-items-center "></div>
+                    </div>
+                  </div>
+                  <div className="flex w-3/4 justify-center border-b-2  mb-16 border-x-2 items-center">
+                    <Link
+                      to={`/prod/${element.Id}`}
+                      onClick={() =>
+                        this.props.updateData({
+                          product: this.state,
+                          index: key,
+                        })
+                      }
+                      className="w-1/2 h-14  rounded-2xl flex mb-8    text-center justify-center items-center bg-[#f79b21] hover:shadow-2xl font-bold "
+                    >
+                      Ürünü Güncelle
+                    </Link>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({});
+function mapStateToProps(data) {
+  const allData = data.data.data;
+  console.log(allData);
+  return { allData };
+}
 
 const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(DataUpdate);
+export default connect(mapStateToProps, { updateData })(DataUpdate);
